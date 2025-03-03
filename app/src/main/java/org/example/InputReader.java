b615ff676c15146d0817a4f236938b6cc62002fe
StupidUsernamee <amirm.mamnoon@gmail.com>
2025-03-03 10:45:26 +0330

    Basic version of grep that searchs a file implemented. project's new features will be added

--- app/src/main/java/org/example/InputReader.java
+++ app/src/main/java/org/example/InputReader.java
@@ -1,4 +1,21 @@
 package org.example;
 
-public class InputReader {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class InputReader  {
+    private static Scanner scanner;
+
+    public static ArrayList<String> readFile(Path path) throws IOException {
+        File file = path.toFile();
+        scanner = new Scanner(file);
+        ArrayList<String> list = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            list.add(scanner.nextLine());
+        }
+        return list;
+    }
 }
--- app/src/main/java/org/example/StringFinder.java
+++ app/src/main/java/org/example/StringFinder.java
@@ -1,4 +1,15 @@
 package org.example;
 
-public class StringFInder {
+import org.example.Utility.SearchResult;
+
+public class StringFinder {
+
+    public static SearchResult findString(String[] fileContent, String toFind) {
+        for (int i = 0; i < fileContent.length; i++) {
+            if (fileContent[i].toLowerCase().contains(toFind)) {
+                return new SearchResult(i+1, fileContent[i]);
+            }
+        }
+        return new SearchResult(-1, null);
+    }
 }
